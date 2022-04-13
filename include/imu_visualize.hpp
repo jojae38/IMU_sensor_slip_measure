a01a4fd7d585012eafafbf0f960a6f213f522c62
jojae38 <58541374+jojae38@users.noreply.github.com>
2022-04-13 17:08:00 +0900

    v1.0.6

--- include/imu_visualize.hpp
+++ include/imu_visualize.hpp
@@ -6,18 +6,37 @@
 #include <sensor_msgs/Imu.h>
 #include <nav_msgs/Odometry.h>
 #include <std_msgs/String.h>
+#include <message_filters/synchronizer.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/sync_policies/approximate_time.h>
 using namespace std;
-#define ppo pose.pose.orientation
+using namespace message_filters;
+const double PI = 3.141592;
 const double Gap = 3.1;
 struct diff_info
 {
     nav_msgs::Odometry Robot_val;
-    nav_msgs::Odometry add_Robot_val;
-    nav_msgs::Odometry adj_Robot_val;
     sensor_msgs::Imu Imu_val;
 
-    geometry_msgs::Pose prev_Q;
-    geometry_msgs::Pose curr_Q;
+    double prev_Robot_yaw;
+    double curr_Robot_yaw;
+    double prev_Imu_yaw;
+    double curr_Imu_yaw;
+
+    int overshoot_robot;
+    int overshoot_imu;
+
+    double prev_modified_Robot_yaw;
+    double curr_modified_Robot_yaw;
+
+    double prev_modified_Imu_yaw;
+    double curr_modified_Imu_yaw;
+
+    double adjust_yaw;
+    sensor_msgs::Imu adjust_d_val;
+    sensor_msgs::Imu slip_d_val;
+
     double curr_Q_yaw;
     double prev_Q_yaw;
 
@@ -29,7 +48,10 @@ struct diff_info
 };
 
 class IMU_visual
-{
+{   public:
+        IMU_visual();
+        ~IMU_visual();
+        void callback(const sensor_msgs::ImuConstPtr &Imu,const nav_msgs::OdometryConstPtr &Robot);
     private:
         // ros
         ros::NodeHandle _nh;
@@ -52,22 +74,37 @@ class IMU_visual
         bool slip_occured;
 
         bool adjust_occured;
+
+        bool first_on;
         
         double _slip_rate;
         int _no_init;
-
+        int Slip_Delay;
+        int Slip_Delay_index;
+        int Ros_rate;
+        
         double init_diff;
         double adjust_diff;
-        
         
         ros::Time start_time;
 
         vector<double> slip_timer;
+        vector<double> slip_yaw;
+        vector<double> Q_yaw_container;
         vector<sensor_msgs::Imu> slip_d_val;
-        geometry_msgs::Pose slip_d;
+
+        message_filters::Subscriber<sensor_msgs::Imu> imu_data_sub;
+        message_filters::Subscriber<nav_msgs::Odometry> robot_data_sub;
+
+        typedef message_filters::sync_policies::ApproximateTime<sensor_msgs::Imu, nav_msgs::Odometry> MySyncPolicy;
+        typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+        // typedef message_filters::Synchronizer<MySyncPolicy> Sync;
+        boost::shared_ptr<Sync> sync_;
+
         int index;
         double slip_time;
         bool slip_time_measured;
+        int count;
         vector<double> init_th;
 
         void update_IMU(const sensor_msgs::Imu::ConstPtr &msg);
@@ -78,14 +115,20 @@ class IMU_visual
         bool time_duration(double sec);
         void get_init_val();
         void run_sequence();
-        void publish_adjust(double x,double y, double z, double w);
+        void adjust_slip(double x,double y, double z, double w);
         void show_slip();
+        void init_prev();
+        void determine_overshoot();
+        void calc_modified_yaw();
         double return_current_time();
         double turn_Quaternion_to_yaw(geometry_msgs::Pose &x,geometry_msgs::Pose &y);
         geometry_msgs::Pose return_sub_pose(geometry_msgs::Pose &A, geometry_msgs::Pose &B);
         geometry_msgs::Pose return_sub_pose(nav_msgs::Odometry &A, sensor_msgs::Imu &B);
+        geometry_msgs::Pose return_plus_pose(nav_msgs::Odometry &A, sensor_msgs::Imu &B);
+
+        geometry_msgs::Pose return_pose(sensor_msgs::Imu &A);
+        geometry_msgs::Pose return_pose(nav_msgs::Odometry &A);
+        
+        
         void turn_curr_to_prev();
-    public:
-        IMU_visual();
-        ~IMU_visual();
 };
--- src/imu_visualize.cpp
+++ src/imu_visualize.cpp
@@ -3,8 +3,10 @@
 IMU_visual::IMU_visual()
 {
     ros::NodeHandle _nh("~");
-    _nh.param("Slip", _slip_rate, 0.3);
-    _nh.param("Slip", _no_init, 1);
+    _nh.param("Ros_rate", Ros_rate, 15);
+    _nh.param("Slip_ddq", _slip_rate, 0.3);
+    _nh.param("notinit", _no_init, 1);
+    _nh.param("Slip_Delay", Slip_Delay, 1);
     slip=false;
     slip_occured=false;
     init_finish=false;
@@ -13,25 +15,38 @@ IMU_visual::IMU_visual()
     adjust_occured=false;
 
     imu_callback=false;
-    robot_callback=true;
+    robot_callback=false;
     
     init_diff=0;
     adjust_diff=0;
-
-    vector<double> temp(3);
-    vector<sensor_msgs::Imu> temp_val(4);
-    slip_timer=temp;
-    slip_d_val=temp_val;
+    count=0;
+    vector<double> empty_con1(3);
+    slip_timer=empty_con1;
+    slip_yaw=empty_con1;
     index=0;
+    Slip_Delay_index=0;
+    vector<sensor_msgs::Imu> empty_con2(3);
+    slip_d_val=empty_con2;
 
+    vector<double> empty_con3(Slip_Delay);
+    Q_yaw_container=empty_con3;
     memset(&diff_info_,0,sizeof(struct diff_info));
     // _pub_robot_adjust=_nh.advertise<geometry_msgs::Pose>("/robot_adjust_byslip",1);
     _pub_differ=_nh.advertise<geometry_msgs::Twist>("/acc_diff",1);
-    _sub_imu_data=_nh.subscribe("/imu_read",1, &IMU_visual::update_IMU, this);
-    _sub_robot_data=_nh.subscribe("/odom",1,&IMU_visual::update_ROBOT,this);
+    // _sub_imu_data=_nh.subscribe("/imu_read",1, &IMU_visual::update_IMU, this);
+    // _sub_robot_data=_nh.subscribe("/odom",1,&IMU_visual::update_ROBOT,this);
+
+    //time sync
+    imu_data_sub.subscribe(_nh,"/imu_read",1);
+    robot_data_sub.subscribe(_nh,"/odom",1);
+    // robot_data_sub
+    
+    // message_filters::Subscriber<sensor_msgs::Imu>imu_data_sub(_nh,"/imu_read",1);
+    // message_filters::Subscriber<nav_msgs::Odometry>robot_data_sub(_nh,"/odom",1);
+    sync_.reset(new Sync(MySyncPolicy(10),imu_data_sub,robot_data_sub));
+    sync_->registerCallback(boost::bind(&IMU_visual::callback,this,_1,_2));    
     //test purpose//
     // _sub_robot_adjust=_nh.subscribe("/robot_adjust_byslip",5,&IMU_visual::adjust_ROBOT,this);
-
     run_sequence();
 }
 IMU_visual::~IMU_visual()
@@ -40,12 +55,12 @@ IMU_visual::~IMU_visual()
 }
 void IMU_visual::run_sequence()
 {   
-    ros::Rate rate(30);
+    ros::Rate rate(Ros_rate);
     
     while(ros::ok())
     {
         if(imu_callback&&robot_callback)
-        {
+        {   init_prev();
             calc_acc();
             if(_no_init)
             {
@@ -55,8 +70,7 @@ void IMU_visual::run_sequence()
             }
             else
             {
-                calc_slip_time();
-                
+                calc_slip_time();   
             }
             show_slip();
             turn_curr_to_prev();
@@ -68,8 +82,8 @@ void IMU_visual::run_sequence()
 void IMU_visual::get_init_val()
 {
     if(init_finish==false)
-    {
-        if(init_th.size()<=30)
+    {   ROS_INFO("dd");
+        if(init_th.size()<=Ros_rate)
         {
             init_th.push_back(diff_info_.curr_Q_yaw);
         }
@@ -93,18 +107,6 @@ void IMU_visual::update_ROBOT(const nav_msgs::Odometry::ConstPtr &msg)
 {
     diff_info_.Robot_val=*msg;
     robot_callback=true;
-    
-    //testing purpose - not connected to robot
-    // diff_info_.adj_Robot_val.ppo.x=diff_info_.Robot_val.ppo.x;
-    // diff_info_.adj_Robot_val.ppo.y=diff_info_.Robot_val.ppo.y;
-    // diff_info_.adj_Robot_val.ppo.z=diff_info_.Robot_val.ppo.z;
-    // diff_info_.adj_Robot_val.ppo.w=diff_info_.Robot_val.ppo.w;
-    ROS_INFO("x: %f y: %f z: %f w: %f ",diff_info_.Robot_val.ppo.x,diff_info_.Robot_val.ppo.y,diff_info_.Robot_val.ppo.z,diff_info_.Robot_val.ppo.w);
-    //connected to robot
-    diff_info_.adj_Robot_val.ppo.x=diff_info_.Robot_val.ppo.x+diff_info_.add_Robot_val.ppo.x;
-    diff_info_.adj_Robot_val.ppo.y=diff_info_.Robot_val.ppo.y+diff_info_.add_Robot_val.ppo.y;
-    diff_info_.adj_Robot_val.ppo.z=diff_info_.Robot_val.ppo.z+diff_info_.add_Robot_val.ppo.z;
-    diff_info_.adj_Robot_val.ppo.w=diff_info_.Robot_val.ppo.w+diff_info_.add_Robot_val.ppo.w;
 }
 void IMU_visual::adjust_ROBOT(const geometry_msgs::Pose::ConstPtr &msg)
 {
@@ -122,8 +124,12 @@ void IMU_visual::calc_slip_time()
     {
         if(abs(diff_info_.curr_ddQ_yaw)>_slip_rate)//?
         {
+            ROS_INFO("ddQ: %f",diff_info_.curr_ddQ_yaw);
+            // ROS_INFO("cu_IMU : %f cu_ROB : %f ",diff_info_.curr_Imu_yaw,diff_info_.curr_Robot_yaw);
+            // ROS_INFO("cu_m_IMU : %f cu_m_ROB : %f ",diff_info_.curr_modified_Imu_yaw,diff_info_.curr_modified_Robot_yaw);
             slip=true;
             slip_timer[0]=return_current_time();
+            slip_yaw[0]=diff_info_.curr_Q_yaw;
             slip_d_val[0]=diff_info_.Imu_val;
             index++;
         }
@@ -146,6 +152,7 @@ void IMU_visual::calc_slip_time()
         if(run)
         {
             slip_timer[index]=return_current_time();
+            slip_yaw[index]=diff_info_.curr_Q_yaw;
             slip_d_val[index]=diff_info_.Imu_val;
             index++;
             run=false;
@@ -155,46 +162,91 @@ void IMU_visual::calc_slip_time()
             slip=false;
             index=0;
             slip_time=slip_timer[2]-slip_timer[0];
-            #define Ori slip_d_val[3].orientation 
-            
-            Ori.x=slip_d_val[2].orientation.x-slip_d_val[0].orientation.x;
-            Ori.y=slip_d_val[2].orientation.y-slip_d_val[0].orientation.y;
-            Ori.z=slip_d_val[2].orientation.z-slip_d_val[0].orientation.z;
-            Ori.w=slip_d_val[2].orientation.w-slip_d_val[0].orientation.w;
-            publish_adjust(Ori.x,Ori.y,Ori.z,Ori.w);
-            ROS_INFO("change x: %f y: %f z: %f w: %f",Ori.x,Ori.y,Ori.z,Ori.w);
+            diff_info_.adjust_yaw=slip_yaw[2]-slip_yaw[0];
+
+            diff_info_.slip_d_val.orientation.x=slip_d_val[2].orientation.x-slip_d_val[0].orientation.x;
+            diff_info_.slip_d_val.orientation.y=slip_d_val[2].orientation.y-slip_d_val[0].orientation.y;
+            diff_info_.slip_d_val.orientation.z=slip_d_val[2].orientation.z-slip_d_val[0].orientation.z;
+            diff_info_.slip_d_val.orientation.w=slip_d_val[2].orientation.w-slip_d_val[0].orientation.w;
+
+            diff_info_.adjust_d_val.orientation.x+=diff_info_.slip_d_val.orientation.x;
+            diff_info_.adjust_d_val.orientation.y+=diff_info_.slip_d_val.orientation.y;
+            diff_info_.adjust_d_val.orientation.z+=diff_info_.slip_d_val.orientation.z;
+            diff_info_.adjust_d_val.orientation.w+=diff_info_.slip_d_val.orientation.w;
+
+            tf::Pose pose;
+            tf::poseMsgToTF(return_pose(diff_info_.adjust_d_val), pose);
+            diff_info_.adjust_yaw=tf::getYaw(pose.getRotation());
+            // ROS_INFO("cu_IMU : %f cu_ROB : %f ",diff_info_.curr_Imu_yaw,diff_info_.curr_Robot_yaw);
+            // ROS_INFO("cu_m_IMU : %f cu_m_ROB : %f ",diff_info_.curr_modified_Imu_yaw,diff_info_.curr_modified_Robot_yaw);
+            // adjust_slip(Ori.x,Ori.y,Ori.z,Ori.w);
+            ROS_INFO("change yaw: %f",diff_info_.adjust_yaw);
         }
     }
 }
 void IMU_visual::calc_acc()
 {
     //testing - diff_info_.Robot_val | Real - diff_info_.adj_Robot_val
-    nav_msgs::Odometry temp_ROBOT_=diff_info_.adj_Robot_val;
+    nav_msgs::Odometry temp_ROBOT_=diff_info_.Robot_val;
     sensor_msgs::Imu temp_IMU_=diff_info_.Imu_val;
     tf::Pose pose;
+// -init_diff;
 
-    diff_info_.curr_Q=return_sub_pose(temp_ROBOT_,temp_IMU_);
-    tf::poseMsgToTF(diff_info_.curr_Q, pose);
-    diff_info_.curr_Q_yaw=tf::getYaw(pose.getRotation())-init_diff;
-    if(adjust_occured)
-    {
-        diff_info_.prev_Q_yaw=diff_info_.curr_Q_yaw;
-        adjust_occured=false;
-    }
-        if(diff_info_.curr_Q_yaw>Gap&&diff_info_.prev_Q_yaw<-Gap)
-        {
-            diff_info_.curr_Q_yaw=diff_info_.prev_Q_yaw;
-        }
-        else if(diff_info_.curr_Q_yaw<-Gap&&diff_info_.prev_Q_yaw>Gap)
-        {
-            diff_info_.curr_Q_yaw=diff_info_.prev_Q_yaw;
-        }
-    // tf::poseMsgToTF(diff_info_.prev_Q, pose);
-    // diff_info_.prev_Q_yaw=tf::getYaw(pose.getRotation())-init_diff;
 
-    diff_info_.curr_dQ_yaw=(diff_info_.curr_Q_yaw-diff_info_.prev_Q_yaw)*5;
-    diff_info_.curr_ddQ_yaw=diff_info_.curr_dQ_yaw-diff_info_.prev_dQ_yaw;
+    tf::poseMsgToTF(return_pose(diff_info_.Robot_val), pose);
+    diff_info_.curr_Robot_yaw=tf::getYaw(pose.getRotation());
+    tf::poseMsgToTF(return_pose(diff_info_.Imu_val), pose);
+    diff_info_.curr_Imu_yaw=tf::getYaw(pose.getRotation());
+    //IMU 센서 값이 로봇보다 높을 시 시나리오 0 그 외에는 시나리오 1
+    determine_overshoot();
+    calc_modified_yaw();
 
+    //로봇 보정 10->6.28
+    diff_info_.curr_modified_Robot_yaw*=0.628;
+    diff_info_.curr_Q_yaw=diff_info_.curr_modified_Imu_yaw-diff_info_.curr_modified_Robot_yaw;
+
+    //slip delay active
+    // Q_yaw_container[Slip_Delay_index]=diff_info_.curr_Q_yaw;
+    // Slip_Delay_index++;
+
+    // if(Slip_Delay_index>=Slip_Delay)
+    // {
+    //     diff_info_.curr_dQ_yaw=0;
+    //     for(int i=0;i<Slip_Delay;i++)
+    //     {
+    //         diff_info_.curr_dQ_yaw+=Q_yaw_container[i]/float(Slip_Delay);
+    //     }
+    //     // diff_info_.curr_dQ_yaw=(Q_yaw_container[Slip_Delay-1]-Q_yaw_container[0])*10;
+    //     diff_info_.curr_ddQ_yaw=diff_info_.curr_dQ_yaw-diff_info_.prev_dQ_yaw;
+    //     Slip_Delay_index=0;
+    // }
+    //slip delay not active
+
+    // 두 각도의 차는 일정하지만 단순 회전만 하는데 dq값과 ddq값이 튀는 현상 관측
+    // 일부러 슬립을 줄 경우 두 각도의 차가 변함 그리고 dq값과 ddq값도 튐 그러나 간혈적으로 슬립이 일어났다고 인식을 못하는 경우 관측
+    diff_info_.curr_dQ_yaw=(diff_info_.curr_Q_yaw-diff_info_.prev_Q_yaw)*Ros_rate;
+    diff_info_.curr_ddQ_yaw=(diff_info_.curr_dQ_yaw-diff_info_.prev_dQ_yaw)*Ros_rate;
+
+    // ROS_INFO("DDQ: %f",diff_info_.curr_ddQ_yaw);
+    // ROS_INFO("IMU : %f ROB : %f ",diff_info_.curr_Imu_yaw,diff_info_.curr_Robot_yaw);
+    // ROS_INFO("adj : %f ini : %f ",diff_info_.adjust_yaw,init_diff);
+    
+    //양쪽 다 360도가 넘어갈 경우 처리과정?
+
+    // if(adjust_occured)
+    // {
+    //     diff_info_.prev_Q_yaw=diff_info_.curr_Q_yaw;
+    //     adjust_occured=false;
+    // }
+    //     if(diff_info_.curr_Q_yaw>Gap&&diff_info_.prev_Q_yaw<-Gap)
+    //     {
+    //         diff_info_.curr_Q_yaw=diff_info_.prev_Q_yaw;
+    //     }
+    //     else if(diff_info_.curr_Q_yaw<-Gap&&diff_info_.prev_Q_yaw>Gap)
+    //     {
+    //         diff_info_.curr_Q_yaw=diff_info_.prev_Q_yaw;
+    //     }
+    
     diff.angular.x=diff_info_.curr_Q_yaw;
     diff.angular.y=diff_info_.curr_dQ_yaw;
     diff.angular.z=diff_info_.curr_ddQ_yaw;
@@ -219,20 +271,14 @@ bool IMU_visual::time_duration(double sec)
     }
     return false;
 }
-void IMU_visual::publish_adjust(double x,double y, double z, double w)
+void IMU_visual::adjust_slip(double x,double y, double z, double w)
 {
-    // geometry_msgs::Pose temp;
-    // temp.orientation.x=x;
-    // temp.orientation.y=y;
-    // temp.orientation.z=z;
-    // temp.orientation.w=w;
-    // _pub_robot_adjust.publish(temp);
-
-    diff_info_.add_Robot_val.ppo.x+=x;
-    diff_info_.add_Robot_val.ppo.y+=y;
-    diff_info_.add_Robot_val.ppo.z+=z;
-    diff_info_.add_Robot_val.ppo.w+=w;
+    // diff_info_.curr_Q.orientation.x-=x;
+    // diff_info_.curr_Q.orientation.y-=y;
+    // diff_info_.curr_Q.orientation.z-=z;
+    // diff_info_.curr_Q.orientation.w-=w;
     adjust_occured=true;
+
     ROS_WARN("ADJUST_Needed");
     fill(slip_timer.begin(),slip_timer.end(),0);
 }
@@ -264,13 +310,15 @@ geometry_msgs::Pose IMU_visual::return_sub_pose(nav_msgs::Odometry &A, sensor_ms
 }
 void IMU_visual::turn_curr_to_prev()
 {
-    diff_info_.prev_Q=diff_info_.curr_Q;
-
     diff_info_.prev_Q_yaw=diff_info_.curr_Q_yaw;
     diff_info_.prev_dQ_yaw=diff_info_.curr_dQ_yaw;
     diff_info_.prev_ddQ_yaw=diff_info_.curr_ddQ_yaw;
+    diff_info_.prev_modified_Imu_yaw=diff_info_.curr_modified_Imu_yaw;
+    diff_info_.prev_modified_Robot_yaw=diff_info_.curr_modified_Robot_yaw;
+    diff_info_.prev_Imu_yaw=diff_info_.curr_Imu_yaw;
+    diff_info_.prev_Robot_yaw=diff_info_.curr_Robot_yaw;
 }
-void IMU_visual::show_slip()
+void IMU_visual::show_slip()//modify needed
 {
     cv::namedWindow("Slip_Occur==RED");
     cv::moveWindow("Slip_Occur==RED",60,0);
@@ -285,4 +333,80 @@ void IMU_visual::show_slip()
         cv::imshow("Slip_Occur==RED",Green_map);
     }
     cv::waitKey(10)==27;
+}
+geometry_msgs::Pose IMU_visual::return_pose(sensor_msgs::Imu &A)
+{
+    geometry_msgs::Pose temp;
+    temp.orientation.x=0;
+    temp.orientation.y=0;
+    temp.orientation.z=A.orientation.z;
+    temp.orientation.w=A.orientation.w;
+    return temp;
+}
+geometry_msgs::Pose IMU_visual::return_pose(nav_msgs::Odometry &A)
+{
+    geometry_msgs::Pose temp;
+    temp.orientation.x=0;
+    temp.orientation.y=0;
+    temp.orientation.z=A.pose.pose.orientation.z;
+    temp.orientation.w=A.pose.pose.orientation.w;
+    return temp;
+}
+
+geometry_msgs::Pose IMU_visual::return_plus_pose(nav_msgs::Odometry &A, sensor_msgs::Imu &B)
+{
+    geometry_msgs::Pose temp;
+
+    temp.orientation.x=A.pose.pose.orientation.x+B.orientation.x;
+    temp.orientation.y=A.pose.pose.orientation.y+B.orientation.y;
+    temp.orientation.z=A.pose.pose.orientation.z+B.orientation.z;
+    temp.orientation.w=A.pose.pose.orientation.w+B.orientation.w;
+
+    return temp;
+}
+void IMU_visual::init_prev()
+{
+    if(first_on)
+    {
+        tf::Pose pose;
+        tf::poseMsgToTF(return_pose(diff_info_.Robot_val), pose);
+        diff_info_.curr_Robot_yaw=tf::getYaw(pose.getRotation());
+        tf::poseMsgToTF(return_pose(diff_info_.Imu_val), pose);
+        diff_info_.curr_Imu_yaw=tf::getYaw(pose.getRotation());
+        diff_info_.prev_Robot_yaw=diff_info_.curr_Robot_yaw;
+        diff_info_.prev_Imu_yaw=diff_info_.curr_Imu_yaw;
+        diff_info_.curr_dQ_yaw=0;
+        diff_info_.curr_ddQ_yaw=0;
+        first_on=false;
+    }
+}
+void IMU_visual::calc_modified_yaw()
+{
+    diff_info_.curr_modified_Imu_yaw=diff_info_.overshoot_imu*PI*2+diff_info_.curr_Imu_yaw;
+    diff_info_.curr_modified_Robot_yaw=diff_info_.overshoot_robot*PI*2+diff_info_.curr_Robot_yaw;
+}
+void IMU_visual::determine_overshoot()
+{
+    if(diff_info_.prev_Imu_yaw<-2.5 && diff_info_.curr_Imu_yaw>2.5)
+    diff_info_.overshoot_imu--;
+    else if(diff_info_.prev_Imu_yaw>2.5&&diff_info_.curr_Imu_yaw<-2.5)
+    diff_info_.overshoot_imu++;
+
+    if(diff_info_.prev_Robot_yaw<-2.5 && diff_info_.curr_Robot_yaw>2.5)
+    diff_info_.overshoot_robot--;
+    else if(diff_info_.prev_Robot_yaw>2.5&&diff_info_.curr_Robot_yaw<-2.5)
+    diff_info_.overshoot_robot++;
+}
+void IMU_visual::callback(const sensor_msgs::ImuConstPtr &Imu,const nav_msgs::OdometryConstPtr &Robot)
+{
+    diff_info_.Robot_val=*Robot;
+    diff_info_.Imu_val=*Imu;
+    imu_callback=true;
+    robot_callback=true;
+    
+    count++;
+    ROS_INFO("%d",count);
+    // ROS_INFO("z: %f w: %f",diff_info_.Robot_val.pose.pose.orientation.z,diff_info_.Robot_val.pose.pose.orientation.w);
+    // ROS_INFO("z: %f w: %f",diff_info_.Imu_val.orientation.z,diff_info_.Imu_val.orientation.w);      
+
 }
